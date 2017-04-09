69a06448e60c63e315b75114bfe03372a6e68b8f
xsd <saleksey67@gmail.com>
2017-04-09 07:22:25 +0300

    TIJ0212

---
+++ src/tij02/HelloDate.java
@@ -0,0 +1,28 @@
+//: object/HelloDate.java
+
+import java.util.*;
+
+/**
+ * Первая программа-пример книги.
+ * Выводит строку и текущее число.
+ *
+ * @author Брюс Эккель
+ * @author www.MindView.net
+ * @version 4.0
+ */
+
+public class HelloDate {
+    /**
+     * Точка входа в класс и приложение
+     *
+     * @param args Массив строковых аргументов
+     * @throws exceptions Исключения не выдаются
+     */
+    public static void main(String[] args) {
+        System.out.println("Привет, сегодня: ");
+        System.out.println(new Date());
+    }
+} /* Output: (55% match)
+Привет, сегодня:
+Wed Oct 05 14:39:36 MDT 2005
+*///:~
