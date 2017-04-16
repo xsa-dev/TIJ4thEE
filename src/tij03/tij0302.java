81ac356300792eda86acb171c99055478a0129ec
xsd <saleksey67@gmail.com>
2017-04-16 22:37:07 +0300

    tij0302 added!

---
+++ src/tij03/tij0302.java
@@ -0,0 +1,29 @@
+package tij03;
+
+/**
+ * Created by xsd on 16.04.2017 with love.
+ * :)
+ * <p>
+ * Example of alliasing
+ *
+ * @author xsd
+ * @author alekseysavin.com
+ * @version 0.1
+ */
+class Val {
+    float attr;
+}
+
+public class tij0302 {
+    public static void main(String[] args) {
+        Val fone = new Val();
+        Val ftwo = new Val();
+        fone.attr = 5.0f;
+        ftwo.attr = 3.1f;
+        System.out.println("fone: " + fone.attr + " ftwo: " + ftwo.attr);
+        fone = ftwo;
+        System.out.println("fone: " + fone.attr + " ftwo: " + ftwo.attr);
+        fone.attr = 5.0f;
+        System.out.println("fone: " + fone.attr + " ftwo: " + ftwo.attr);
+    }
+}
