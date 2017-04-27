08afa62721bbe5b801ed01cd3587b6d924761df5
xsd <saleksey67@gmail.com>
2017-04-27 23:45:36 +0300

    tij0308 Литералы Показываю что шестнадцатеричная и восмеричная записи могут использоваться с типом long. Использую метод Long.toBinaryString() для выводов результатов.

---
+++ src/tij03/tij0308.java
@@ -0,0 +1,19 @@
+package tij03;
+
+/**
+ * Created by xsd on 27.04.2017 with love.
+ * :)
+ *
+ * @author xsd
+ * @author alekseysavin.com
+ * @version 0.1
+ */
+public class tij0308 {
+    public static void main(String[] args) {
+        long eight = 0177;
+        long sixth = 0x2f;
+
+        System.out.println(Long.toBinaryString(sixth)); //16
+        System.out.println(Long.toBinaryString(eight)); //8
+    }
+}
