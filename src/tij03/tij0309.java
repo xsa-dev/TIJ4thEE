c01fa28cdce85189cf9cc26c0dad53f796955d4a
xsd <saleksey67@gmail.com>
2017-04-28 23:54:01 +0300

    tij0309 Вывожу на экран Максимальное и Минимальное чилсла в экспоненциальной записи для типов float и double

---
+++ src/tij03/tij0309.java
@@ -0,0 +1,19 @@
+package tij03;
+
+/**
+ * Created by xsd on 27.04.2017 with love.
+ * :)
+ *
+ * @author xsd
+ * @author alekseysavin.com
+ * @version 0.1
+ */
+
+public class tij0309 {
+    public static void main(String[] args) {
+        float expFloat = 1.39e-43f;
+        double expDouble = 47e47;
+        System.out.println(Math.max(expDouble, expFloat));
+        System.out.println(Math.min(expDouble, expFloat));
+    }
+}
