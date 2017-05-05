256e5bd6bc5ec1442b649f0245ec8f3784d94910
xsd <saleksey67@gmail.com>
2017-05-05 23:43:35 +0300

    tij0401 added

---
+++ src/tij04/tij0401.java
@@ -0,0 +1,17 @@
+package tij04;
+
+/**
+ * Created by xsd on 05.05.2017 with love.
+ * :)
+ *
+ * @author xsd
+ * @author alekseysavin.com
+ * @version 0.1
+ */
+public class tij0401 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
