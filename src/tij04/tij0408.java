4fcda0bdf217875d910e4c330c3ba43c86dd1f7a
xsd <saleksey67@gmail.com>
2017-05-13 22:44:23 +0300

    tij0408 - удалите break и посмотрите что происходит. case продолжает обрабатывать все исходящие из него операции

--- src/tij04/tij0408.java
+++ src/tij04/tij0408.java
@@ -6,17 +6,22 @@ import java.util.Random;
  * Created by User on 13.05.2017.
  */
 public class tij0408 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Random r = new Random();
-            for (int i = 0; i <= 4; i++) {
-                int c = r.nextInt(26);
-                switch (c) {
-                    case (0): System.out.println(c); break;
-                    case (1): System.out.println(c); break;
-                    case (2): System.out.println(c); break;
-                    case (3): System.out.println(c); break;
-                    case (4): System.out.println(c); break;
-                }
+        for (int i = 0; i <= 4; i++) {
+            int c = r.nextInt(4);
+            switch (c) {
+                case (0):
+                    System.out.println(c);   
+                case (1):
+                    System.out.println(c);
+                case (2):
+                    System.out.println(c);
+                case (3):
+                    System.out.println(c);
+                case (4):
+                    System.out.println(c);
             }
         }
     }
+}
