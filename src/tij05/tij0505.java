911d0db0920ffa2c71571c0c6572b2d672aa382d
xsd <saleksey67@gmail.com>
2017-05-24 22:42:12 +0300

    tij0505

---
+++ src/tij05/tij0505.java
@@ -0,0 +1,15 @@
+package tij05;
+
+/**
+ * Created by xsd on 24.05.2017 with love.
+ * :)
+ *
+ * @author xsd
+ * @author alekseysavin.com
+ * @version 0.1
+ */
+public class tij0505 {
+    public static void main(String[] args) {
+        tij0505 ti = new tij0505();
+    }
+}
