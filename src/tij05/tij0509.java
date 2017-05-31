e8625957e3e5bffb71c895afb0bf18a4517e882d
xsd <saleksey67@gmail.com>
2017-06-01 02:56:04 +0300

    tij0509: Подготовил класс с двумя (перезагруженными) конструктурами. Используя ключевое слово this вызвал второй класс из первого.

---
+++ src/tij05/tij0509.java
@@ -0,0 +1,23 @@
+package tij05;
+
+/**
+ * Created by xsd on 01.06.2017 with love.
+ * :)
+ *
+ * @author xsd
+ * @author alekseysavin.com
+ * @version 0.1
+ */
+public class tij0509 {
+    tij0509(String x, String y) {
+        System.out.println("tij0509 row:13 " + x + " " + y);
+    }
+
+    tij0509() {
+        this("ok", "boss");
+    }
+
+    public static void main(String[] args) {
+        tij0509 t = new tij0509();
+    }
+}
